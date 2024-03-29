b1ab65110166c9e415d6aefb94848dca175acef1
P-Danov <121188931+P-Danov@users.noreply.github.com>
2024-03-29 14:21:44 +0100

    Add files via upload

---
+++ api/index.ts
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose')
+const Score = require('./score.model.js')
+const port =  3000;
+
+
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+
+
+
+
+
+
+
+mongoose.connect('mongodb+srv://mrocznymrok:Killer666666@meokdatabase.tqetnqk.mongodb.net/?retryWrites=true&w=majority&appName=meokDatabase')
+.then(()=>{
+    console.log('connected to meok db')
+    app.listen(port,async()=>{
+        console.log('server is running on port '+ port)
+        
+    });
+})
+.catch(()=>{
+    console.log('connection fail')
+})
+
+app.use(express.static('public'));
+app.use(express.json());
+
+
+
+app.get("/home",async(req,res)=>{
+
+    try{
+        
+        res.sendFile("index.html", { root: __dirname})
+        console.log('1')
+
+            app.get("/scoresList",async(req,res)=>{
+
+            const importedScore = await Score.find();
+            res.send(importedScore) 
+            console.log(importedScore)
+            console.log('2')
+            }) 
+    }catch(error){
+        console.log(error.message)
+        res.status(500).json({message:error.message})
+    }
+               
+})
+
+app.post("/scores", async(req,res) => {
+    try{
+         const score = await Score.updateOne({
+            scoreName:req.body.oldObject.scoreName,score:req.body.oldObject.score},
+            {scoreName:req.body.newObject.scoreName,score:req.body.newObject.score})
+         res.status(200).json(score)
+        
+    }catch(error){
+        console.log(error.message)
+        res.status(500).json({message:error.message})
+    }
+})
+  
+module.exports = app;
+
+
